d1c3456328e1895b6739824936c10f29ffb9c564
Shreya S D <shreyasd398@gmail.com>
2026-01-03 23:41:22 +0530

    Add simple calculator for sum and difference

---
+++ Python/sum_difference.py
@@ -0,0 +1,7 @@
+def add(a, b): return a + b
+def sub(a, b): return a - b
+print("---Simple Calculator ---")
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+print(f"Sum: {add(num1, num2)}")
+print(f"Difference: {sub(num1, num2)}")
