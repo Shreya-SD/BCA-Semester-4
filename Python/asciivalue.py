5931ced043976dd9e5731663d2f84a59cd54cd3f
Shreya S D <shreyasd398@gmail.com>
2026-01-16 23:28:11 +0530

    Add ASCII value converter in asciivalue.py

---
+++ Python/asciivalue.py
@@ -0,0 +1,3 @@
+c=input("enter the character that needed to be converted into its ASCII value:")
+a=ord(c) # the built-in ord() function to get the ASCII value of a character.
+print("ASCII value of",c,"is",a)
